b2b664410f9e29e2a8691ba848d6aa7cf62ead62
Tommy McMichen <mcmichen@u.northwestern.edu>
2026-05-03 12:23:41 -0500

    [strip-tbaa] Add basic TBAA metadata stripping tool

---
+++ src/strip-tbaa/main.cc
@@ -0,0 +1,65 @@
+#include <string>
+
+#include "llvm/Bitcode/BitcodeWriter.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/ToolOutputFile.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+static cl::opt<std::string> opt_inpath(cl::Positional,
+                                       cl::desc("Input file"),
+                                       cl::Required);
+static cl::opt<std::string> opt_outpath("o",
+                                        cl::desc("Output file"),
+                                        cl::value_desc("filename"));
+
+int main(int argc, char **argv) {
+  cl::ParseCommandLineOptions(argc, argv, "Strip TBAA metadata\n");
+
+  StringRef inpath = opt_inpath;
+  StringRef outpath = opt_outpath;
+
+  LLVMContext context;
+  SMDiagnostic error;
+  auto module = parseIRFile(inpath, error, context);
+  if (!module) {
+    error.print(argv[0], errs());
+    return 1;
+  }
+
+  for (auto &F : *module)
+    for (auto &BB : F)
+      for (auto &I : BB)
+        I.setMetadata(LLVMContext::MD_tbaa, nullptr);
+
+  if (outpath.empty()) {
+    module->print(outs(), nullptr);
+  } else {
+
+    bool is_bitcode = outpath.ends_with(".bc");
+    auto flag = is_bitcode ? sys::fs::OF_None : sys::fs::OF_Text;
+
+    std::error_code code;
+    ToolOutputFile outfile(outpath, code, flag);
+    if (code) {
+      errs() << code.message() << "\n";
+      return 1;
+    }
+
+    if (is_bitcode) {
+      WriteBitcodeToFile(*module, outfile.os());
+    } else {
+      module->print(outfile.os(), nullptr);
+    }
+
+    outfile.keep();
+  }
+
+  return 0;
+}
