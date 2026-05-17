02d434db1bb7314e0d73706b6d1fbd7c7455f5d1
Tommy McMichen <mcmichen@u.northwestern.edu>
2026-05-17 14:55:46 -0500

    Add cast using statements

---
+++ include/nifty/cast.hh
@@ -0,0 +1,12 @@
+#include <llvm/Support/Casting.h>
+
+namespace nifty {
+
+using llvm::cast;
+using llvm::cast_or_null;
+using llvm::dyn_cast;
+using llvm::dyn_cast_or_null;
+using llvm::isa;
+using llvm::isa_and_nonnull;
+
+} // namespace nifty
