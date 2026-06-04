64f43d3ae19cb6e4e6ed00557e5705ba8c9182ac
Tommy McMichen <mcmichen@u.northwestern.edu>
2026-06-04 08:41:02 -0500

    Updated the assertion print format

--- include/nifty/assert.hh
+++ include/nifty/assert.hh
@@ -8,21 +8,36 @@
 
 #include "nifty/print.hh"
 
-#define _NIFTY_ASSERT(pretty_func, pretty_line, c, msg...)                     \
+#define _NIFTY_ASSERT(func, file, line, c, msg...)                             \
   if (!(c)) {                                                                  \
-    nifty::println("\n");                                                      \
-    nifty::println("\x1b[31m====================================");            \
-    nifty::println("\x1b[1;31mNifty Assert Failed!\x1b[0m");                   \
-    nifty::println("  \x1b[1;33m", msg, "\x1b[0m");                            \
-    nifty::println("  in \x1b[1;35m", pretty_func, "\x1b[0m");                 \
-    nifty::println("  at \x1b[1;35m", pretty_line, "\x1b[0m");                 \
-    nifty::println("\x1b[31m====================================\x1b[0m");     \
-    nifty::println();                                                          \
+    ::nifty::println();                                                        \
+    ::nifty::println(::nifty::Colors::RED,                                     \
+                     ::nifty::Style::BOLD,                                     \
+                     "Nifty Assertion Failed:",                                \
+                     ::nifty::Colors::RESET);                                  \
+    ::nifty::println(::nifty::Colors::YELLOW,                                  \
+                     ::nifty::Style::BOLD,                                     \
+                     msg,                                                      \
+                     ::nifty::Colors::RESET);                                  \
+    ::nifty::println(::nifty::Colors::CYAN,                                    \
+                     ::nifty::Style::BOLD,                                     \
+                     file,                                                     \
+                     ::nifty::Colors::RESET,                                   \
+                     ":",                                                      \
+                     ::nifty::Colors::CYAN,                                    \
+                     ::nifty::Style::BOLD,                                     \
+                     line,                                                     \
+                     ::nifty::Colors::RESET);                                  \
+    ::nifty::println(nifty::Colors::MAGENTA,                                   \
+                     ::nifty::Style::BOLD,                                     \
+                     func,                                                     \
+                     ::nifty::Colors::RESET);                                  \
+    ::nifty::println();                                                        \
     assert(c);                                                                 \
   }
 
 #define NIFTY_ASSERT(c, msg...)                                                \
-  _NIFTY_ASSERT(__PRETTY_FUNCTION__, __LINE__, c, msg)
+  _NIFTY_ASSERT(__PRETTY_FUNCTION__, __FILE__, __LINE__, c, msg)
 
 #define NIFTY_UNREACHABLE(msg...) NIFTY_ASSERT(false, msg)
 
